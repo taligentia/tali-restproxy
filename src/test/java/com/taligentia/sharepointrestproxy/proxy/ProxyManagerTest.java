5110e7988d5d6482bc5651a50e9b9768121fa2eb
Dominique Bejean <dominique.bejean@eolya.fr>
2024-06-29 23:46:35 +0200

    ProxyHttpClient : log & tests

---
+++ src/test/java/com/taligentia/sharepointrestproxy/proxy/ProxyManagerTest.java
@@ -0,0 +1,25 @@
+package com.taligentia.sharepointrestproxy.proxy;
+import org.junit.Test;
+
+public class ProxyManagerTest {
+    @Test
+    public void testDoGet() throws Exception {
+
+        System.setProperty("java.security.auth.login.config", "src/test/resources/login.conf");
+        System.setProperty("java.security.krb5.conf", "src/test/resources/krb5.conf");
+        System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+
+        String authMethod = "kerberos";
+        String user = "";
+        String passwd = "";
+        String domain = "TALIWIN";
+        String url = "https://win2016-sp.taliwin.com/sites/kamare/_api/web/title";
+
+        ProxyHttpClient httpClient = new ProxyHttpClient();
+        httpClient.setAcceptHeader("application/json;odata=verbose");
+        httpClient.setSslVerification(false);
+        httpClient.doGet(authMethod, user, passwd, domain, url);
+        System.out.println(httpClient.getResponse());
+    }
+}
