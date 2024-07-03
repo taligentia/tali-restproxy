8572096c4b9c2c5f51736e29a8af83438c9a75ad
Dominique Bejean <dominique.bejean@eolya.fr>
2024-07-03 11:39:33 +0200

    Ajout d'une possibilité de mise en place d'un script d'initialisation

--- dockerimage/entrypoint.sh
+++ dockerimage/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
 
-# tail -f /dev/null
-
-cd /app && java $JAVA_OPTS --illegal-access=debug -jar $RESTPROXY_JAR server config.yml
-
-touch /app/run.log
-tail -f /app/run.log
+cd /app
+if [ -f "./init.sh" ]; then
+    echo "Launch initialisation script"
+    dos2unix ./init.sh
+    chmod +x ./init.sh
+    source ./init.sh
+fi
+java $JAVA_OPTS --illegal-access=debug -jar $RESTPROXY_JAR server config.yml
+#touch /app/run.log && tail -f /app/run.log
